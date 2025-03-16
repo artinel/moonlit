b74da6983cd84f87a08f45f5e7484e5f947e6d91
artinel <artinel@proton.me>
2025-03-16 20:28:18 +0330

    Load global css in splash window

--- src/ui/splash_window.c
+++ src/ui/splash_window.c
@@ -8,5 +8,6 @@ void splash_window_activate(AdwApplication* app){
 	GObject* window = get_object(builder, "window");
 
 	gtk_window_set_application(GTK_WINDOW(window), GTK_APPLICATION(app));
+	load_css(GTK_WIDGET(window), "/css/global-style");
 	gtk_widget_set_visible(GTK_WIDGET(window), true);
 }
