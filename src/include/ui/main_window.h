552551b675292be230b85297b5dd84ba6bfcd7d0
artinel <artinel@proton.me>
2025-03-19 22:01:28 +0330

    Load main window after loading is completed in splash window

---
+++ src/include/ui/main_window.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_WINDOW_H
+#define _MAIN_WINDOW_H
+
+#include <libadwaita-1/adwaita.h>
+
+void main_window_activate(AdwApplication* app);
+
+#endif
--- src/ui/main_window.c
+++ src/ui/main_window.c
@@ -1,17 +1,14 @@
 #include <libadwaita-1/adwaita.h>
 //local includes
-#include <ui/splash_window.h>
 #include <ui/main_window.h>
 #include <utils.h>
-#include <unistd.h>
 
-void splash_window_activate(AdwApplication* app){
-	GtkBuilder* builder = load_ui("/ui/splash");
+void main_window_activate(AdwApplication* app){
+	GtkBuilder* builder = load_ui("/ui/main");
 	GObject* window = get_object(builder, "window");
 
 	gtk_window_set_application(GTK_WINDOW(window), GTK_APPLICATION(app));
 	load_css(GTK_WIDGET(window), "/css/global-style");
 	gtk_widget_set_visible(GTK_WIDGET(window), true);
-	gtk_window_destroy(GTK_WINDOW(window));
-	main_window_activate(app);
+
 }
